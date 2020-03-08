c73346dccd5a83a3f52e695abf533fcdf3254e18
anguy212 <32521966+anguy212@users.noreply.github.com>
2020-03-08 01:22:21 -0800

    first front end layout in with Sick/Healthy Button added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -9,10 +9,16 @@ const instructions = Platform.select({
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+        <View>
+        <Button title = "Sick"/>
+          <Button title = "Healthy"/>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>Location will be here once loaded!</Text>
+          <Text style={styles.instructions}>{instructions}</Text>
+        </View>
       </View>
     );
   }
